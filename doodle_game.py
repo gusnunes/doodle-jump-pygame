365cfbff14ca10e1f34c8a125cb6f3206705ea0f
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-02-12 04:02:29 -0300

    Adicionando clock e reorganizando melhor o código

--- doodle_game.py
+++ doodle_game.py
@@ -2,18 +2,24 @@ import pygame
 
 class DoodleJumpGame:
     def __init__(self, width, height):
-        self.width  = width
-        self.height = height
         pygame.init()
         pygame.display.set_caption("Doodle Jump")
-        self.screen = pygame.display.set_mode((width,height))   
+
+        self.width  = width
+        self.height = height
+        self.screen = pygame.display.set_mode((width,height))
+        self.clock  = pygame.time.Clock() 
+    
+    def run(self):
+        pass
 
 def main():
     # definindo tamanho da tela
     WIDTH = 640
     HEIGH = 480
 
-    DoodleJumpGame(WIDTH,HEIGH)
+    game = DoodleJumpGame(WIDTH,HEIGH)
+    game.run()
 
 if __name__ == "__main__":
     main()
