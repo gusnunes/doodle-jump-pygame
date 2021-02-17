fc82906ff3907b6c7e5e7e9a2f0e741522f1da0f
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-02-17 17:31:33 -0300

    Método que controla o fluxo do jogo

--- doodle_game.py
+++ doodle_game.py
@@ -1,17 +1,28 @@
 import pygame
+from Doodler import Doodler
 
 class DoodleJumpGame:
     def __init__(self, width, height):
         pygame.init()
         pygame.display.set_caption("Doodle Jump")
 
-        self.width  = width
-        self.height = height
-        self.screen = pygame.display.set_mode((width,height))
-        self.clock  = pygame.time.Clock() 
+        self.width   = width
+        self.height  = height
+        self.screen  = pygame.display.set_mode((width,height))
+        self.clock   = pygame.time.Clock()
+        self.doodler = Doodler() 
     
     def run(self):
-        pass
+        self.clock.tick(8)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            
+            if event.type == pygame.KEYDOWN:
+                self.doodler.key_pressed(pygame.key.get_pressed())
+        
+        pygame.display.update()
 
 def main():
     # definindo tamanho da tela
