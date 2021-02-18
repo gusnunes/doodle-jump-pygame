fcf293b73f8cdbad0ed19598a3d6e0bf52fd75c4
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-02-18 20:22:24 -0300

    Organizando o que faz parte da classe Screen

--- doodle_game.py
+++ doodle_game.py
@@ -1,35 +1,32 @@
 import pygame
-from Doodler import Doodler
+from doodler import Doodler
+from screen import Screen
 
 class DoodleJumpGame:
-    def __init__(self, width, height):
+    def __init__(self):
         pygame.init()
-        pygame.display.set_caption("Doodle Jump")
 
-        self.width   = width
-        self.height  = height
-        self.screen  = pygame.display.set_mode((width,height))
+        self.screen  = Screen()
         self.clock   = pygame.time.Clock()
         self.doodler = Doodler() 
     
     def run(self):
-        self.clock.tick(8)
+        while True:
+            self.clock.tick(8)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                
+                if event.type == pygame.KEYDOWN:
+                    self.doodler.key_pressed(pygame.key.get_pressed())
             
-            if event.type == pygame.KEYDOWN:
-                self.doodler.key_pressed(pygame.key.get_pressed())
-        
-        pygame.display.update()
+            self.screen.draw_background()
+            self.screen.update()
+            
 
 def main():
-    # definindo tamanho da tela
-    WIDTH = 640
-    HEIGH = 480
-
-    game = DoodleJumpGame(WIDTH,HEIGH)
+    game = DoodleJumpGame()
     game.run()
 
 if __name__ == "__main__":
