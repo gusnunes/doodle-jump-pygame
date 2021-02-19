3ca6115f4b7031e530e95a088d25096a47f40dcd
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-02-19 02:50:03 -0300

    Mecanica do movimento

--- doodle_game.py
+++ doodle_game.py
@@ -8,20 +8,28 @@ class DoodleJumpGame:
 
         self.screen  = Screen()
         self.clock   = pygame.time.Clock()
-        self.doodler = Doodler() 
+        self.doodler = Doodler(Screen.WIDTH/2, Screen.HEIGHT/2)
+    
+    def key_pressed(self, key):
+        if key[pygame.K_RIGHT] or key[pygame.K_LEFT]:
+            self.doodler.key_pressed(key)
     
     def run(self):
         while True:
-            self.clock.tick(8)
+            self.clock.tick(30)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                 
-                if event.type == pygame.KEYDOWN:
+                if event.type == pygame.KEYDOWN:   # pressinou e soltou a tecla
                     self.doodler.key_pressed(pygame.key.get_pressed())
-            
+                
+            self.key_pressed(pygame.key.get_pressed())   # manteve a tecla pressionada
             self.screen.draw_background()
+            self.screen.draw_sprites(self.doodler.image, self.doodler.rect)
+
+            self.doodler.update()
             self.screen.update()
             
 
