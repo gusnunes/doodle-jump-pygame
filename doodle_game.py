89af17a397097f039824dd3ee2d62c851d6486ef
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-02-21 03:47:26 -0300

    Adicionando comentarios

--- doodle_game.py
+++ doodle_game.py
@@ -12,6 +12,7 @@ class DoodleJumpGame:
         self.clock   = pygame.time.Clock()
         self.doodler = Doodler(Screen.WIDTH/2, Screen.HEIGHT/2)
     
+    # Jogador aparece do lado oposto ao passar pela borda
     def check_border(self):
         if self.doodler.rect.centerx > self.screen.WIDTH:
             self.doodler.rect.centerx = 0
@@ -19,9 +20,7 @@ class DoodleJumpGame:
         elif self.doodler.rect.centerx < 0:
             self.doodler.rect.centerx = self.screen.WIDTH
 
-    
     def key_pressed(self, key):
-        # TODO: colocar WASD para controlar movimento e shoot
         if key[pygame.K_RIGHT] or key[pygame.K_d]:
             self.doodler.right_movement()
 
@@ -38,17 +37,15 @@ class DoodleJumpGame:
                 
                 if event.type == pygame.KEYDOWN:   # pressinou e soltou a tecla
                     key = pygame.key.get_pressed()
-                    if key[pygame.K_UP] or key[pygame.K_SPACE]:
+                    if key[pygame.K_UP] or key[pygame.K_SPACE] or key[pygame.K_w]:
                         self.doodler.shoot()
-                    
-            
+                      
             self.key_pressed(pygame.key.get_pressed())   # manteve a tecla pressionada
             self.screen.draw_background()
             self.check_border()
             self.screen.draw_doodler(self.doodler.image, self.doodler.rect)
             self.screen.update()
             
-
 def main():
     game = DoodleJumpGame()
     game.run()
