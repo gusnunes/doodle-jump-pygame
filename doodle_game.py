4137ae55514f3a6ea144ec316156572557ef437d
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-02-21 03:26:29 -0300

    Checa se o Doodler passou das bordas

--- doodle_game.py
+++ doodle_game.py
@@ -3,6 +3,8 @@ from doodler import Doodler
 from screen import Screen
 
 class DoodleJumpGame:
+    FPS = 30
+
     def __init__(self):
         pygame.init()
 
@@ -10,7 +12,16 @@ class DoodleJumpGame:
         self.clock   = pygame.time.Clock()
         self.doodler = Doodler(Screen.WIDTH/2, Screen.HEIGHT/2)
     
+    def check_border(self):
+        if self.doodler.rect.centerx > self.screen.WIDTH:
+            self.doodler.rect.centerx = 0
+        
+        elif self.doodler.rect.centerx < 0:
+            self.doodler.rect.centerx = self.screen.WIDTH
+
+    
     def key_pressed(self, key):
+        # TODO: colocar WASD para controlar movimento e shoot
         if key[pygame.K_RIGHT]:
             self.doodler.right_movement()
 
@@ -19,7 +30,7 @@ class DoodleJumpGame:
     
     def run(self):
         while True:
-            self.clock.tick(30)
+            self.clock.tick(DoodleJumpGame.FPS)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -29,12 +40,12 @@ class DoodleJumpGame:
                     key = pygame.key.get_pressed()
                     if key[pygame.K_UP] or key[pygame.K_SPACE]:
                         self.doodler.shoot()
-
+                    
+            
             self.key_pressed(pygame.key.get_pressed())   # manteve a tecla pressionada
             self.screen.draw_background()
-            self.screen.draw_sprites(self.doodler.image, self.doodler.rect)
-
-            self.doodler.update()
+            self.check_border()
+            self.screen.draw_doodler(self.doodler.image, self.doodler.rect)
             self.screen.update()
             
 
