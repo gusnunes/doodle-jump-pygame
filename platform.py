2c1c964044864e0d94be807456ede50621d7b73d
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-02-22 03:13:55 -0300

    Adicionando a classe da Plataforma

---
+++ platform.py
@@ -0,0 +1,16 @@
+# Classe que implementa a plataforma onde o Doodler pula sobre
+import pygame
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, coord_x, coord_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = [
+            pygame.image.load("images/platform_green.png").convert_alpha()
+        ]
+
+        self.image = self.images[0]   # primeira plataforma de teste (somente a verde)
+        self.rect = self.image.get_rect(center=(coord_x,coord_y))
+        self.mask = pygame.mask.from_surface(self.image)   # usado para detectar colisao
+    
+    def update():
+        pass
